fe7621133efe1f3c210f670aae6cb8cb7a211afb
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-09 10:52:20 -0600

    Add comments to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  #shelters
   get '/', to: 'welcome#index'
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
+  #pets
   get '/pets', to: 'pets#index'
   get '/pets/new', to: 'pets#new'
   post '/shelters', to: 'pets#create'
