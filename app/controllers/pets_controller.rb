5943245802d66782631cd6479d4017e2bd045aba
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-08 20:32:08 -0600

    Code create method to create new pet object, save it, and redirect to index

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,15 @@
 class PetsController < ApplicationController
-  def index
+  def index; end
 
+  def create
+    pet = Pet.new({
+                            image: params[:pet][:image],
+                            name: params[:pet][:name],
+                            age: params[:pet][:age],
+                            sex: params[:pet][:sex],
+                            shelter: params[:pet][:shelter]
+                          })
+    pet.save
+    redirect_to '/pets'
   end
 end
