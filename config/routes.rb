af1294bc226a2a7025e2c6a2919e104a19958b3b
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-10 16:06:10 -0600

    Create post route for create shelter pets

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   delete '/shelters/:id', to: 'shelters#destroy'
   get '/shelters/:id/pets', to: 'shelters#pets'
   get 'shelters/:id/pets/new', to: 'shelters#new_pet'
+  post '/shelters/:id/pets', to: 'shelters#create_pet'
   #pets
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
