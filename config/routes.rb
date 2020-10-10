24cf537efa35d3daf5f996e5876dac7eac6c25e1
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-10 16:32:26 -0600

    Change routes to use new shelter pets controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,11 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
-  get '/shelters/:id/pets', to: 'shelters#pets'
-  get 'shelters/:id/pets/new', to: 'shelters#new_pet'
-  post '/shelters/:id/pets', to: 'shelters#create_pet'
   #pets
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
+  #shelter pets
+  get '/shelters/:id/pets', to: 'shelter_pets#pets'
+  get 'shelters/:id/pets/new', to: 'shelter_pets#new_pet'
+  post '/shelters/:id/pets', to: 'shelter_pets#create_pet'
 end
