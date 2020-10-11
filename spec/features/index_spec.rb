907968429dd5cf6138afd07cb413265cd274e57f
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-10 21:29:30 -0600

    Add delete button to pets index page

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -33,13 +33,13 @@ describe 'As a visitor' do
                          name: 'Test_dog',
                          age: 5,
                          sex: 'male',
-                         shelter_id: "#{shelter_1.id}")
+                         shelter_id: shelter_1.id.to_s)
 
       pet_2 = Pet.create(image: 'lib/assets/test2_image',
                          name: 'Test_dog_2',
                          age: 10,
                          sex: 'felmale',
-                         shelter_id: "#{shelter_1.id}")
+                         shelter_id: shelter_1.id.to_s)
 
       visit '/pets'
       expect(page).to have_xpath("//img[contains(@src,'#{pet_1.image}')]")
@@ -52,6 +52,24 @@ describe 'As a visitor' do
       expect(page).to have_content("Age: #{pet_2.age}")
       expect(page).to have_content("Sex: #{pet_2.sex}")
       expect(page).to have_content("Current Shelter Name: #{pet_2.shelter.name}")
+    end
+  end
+  describe 'When I visit the pets index page or a shelter pets index page Next to every pet, I see a link to delete that pet' do
+    it 'When I click the link I should be taken to the pets index page where I no longer see that pet' do
+      shelter_1 = Shelter.create(name: 'Dumb Friends League',
+        address: '123 ABC Street',
+        city: 'Denver',
+        state: 'Colorado',
+        zip: '12345')
+        pet_1 = Pet.create(image: 'lib/assets/test_image',
+          name: 'Test_dog',
+          age: 5,
+          sex: 'male',
+          shelter_id: shelter_1.id.to_s)
+          visit '/pets'
+      find('#delete_button').click
+      expect(current_path).to eq('/pets')
+      expect(page).not_to have_content(pet_1.name)
     end
   end
 end
