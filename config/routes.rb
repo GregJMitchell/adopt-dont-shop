1b4baa4f0e18c87769ed74bd53b620b50e62292c
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-12 16:33:16 -0600

    Create route for create user action

--- config/routes.rb
+++ config/routes.rb
@@ -13,13 +13,14 @@ Rails.application.routes.draw do
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
   get '/pets/:id/edit', to: 'pets#edit'
-  patch 'pets/:id', to: 'pets#update'
-  delete 'pets/:id', to: 'pets#destroy'
+  patch '/pets/:id', to: 'pets#update'
+  delete '/pets/:id', to: 'pets#destroy'
   #shelter pets
   get '/shelters/:id/pets', to: 'shelter_pets#pets'
-  get 'shelters/:id/pets/new', to: 'shelter_pets#new_pet'
+  get '/shelters/:id/pets/new', to: 'shelter_pets#new_pet'
   post '/shelters/:id/pets', to: 'shelter_pets#create_pet'
   #users
   get '/users/new', to: 'users#new'
-  get 'users/:id', to: 'users#show'
+  get '/users/:id', to: 'users#show'
+  post '/users', to: 'users#create'
 end
