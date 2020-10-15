03fae27163f395da96af27a34bb68222060f192e
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-15 17:58:58 -0600

    Update view pages and controllers for flash messages

--- app/controllers/shelter_reviews_controller.rb
+++ app/controllers/shelter_reviews_controller.rb
@@ -5,9 +5,39 @@ class ShelterReviewsController < ApplicationController
     end
 
     def create
+        @shelter = Shelter.find(params[:id])
         user = User.find_by(name: params[:review][:username])
-        
-        review = Review.new(
+        if user == nil
+            flash[:notice] = "Review not created: Required information missing."
+            render :new
+        else
+            review = Review.new(
+                title: params[:review][:title],
+                rating: params[:review][:rating],
+                content: params[:review][:content],
+                username: params[:review][:username],
+                image: params[:review][:image],
+                shelter_id: params[:id],
+                user_id: user.id
+            )
+            if review.save
+                redirect_to "/shelters/#{review.shelter.id}"
+            else
+                flash[:notice] = "Review not created: Required information missing."
+                render :new
+            end
+        end
+    end
+
+    def edit
+        @review = Review.find(params[:id])
+        @shelter = @review.shelter
+    end
+
+    def update
+        review = Review.find(params[:id])
+        user = User.find_by(name: params[:review][:username])
+        review.update(
             title: params[:review][:title],
             rating: params[:review][:rating],
             content: params[:review][:content],
@@ -17,12 +47,7 @@ class ShelterReviewsController < ApplicationController
             user_id: user.id
         )
         
-    review.save
-    redirect_to "/shelters/#{review.shelter.id}"
-    end
-
-    def edit
-        @review = Review.find(params[:id])
-        @shelter = @review.shelter
+        review.save
+        redirect_to "/shelters/#{review.shelter.id}"
     end
 end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,5 +1,4 @@
 class Shelter < ApplicationRecord
   has_many :pets, dependent: :destroy
   has_many :reviews, dependent: :destroy
-  has_many :users, through: :reviews
 end
