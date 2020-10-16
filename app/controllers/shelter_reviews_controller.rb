b870c7842249384dc5593c7820028a90e4045794
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-15 19:48:23 -0600

    Add flash messages to update action

--- app/controllers/shelter_reviews_controller.rb
+++ app/controllers/shelter_reviews_controller.rb
@@ -35,21 +35,31 @@ class ShelterReviewsController < ApplicationController
     end
 
     def update
-        
-        review = Review.find(params[:id])
-        user = User.find_by(name: params[:review][:username])
-        review.update({
-            title: params[:review][:title],
-            rating: params[:review][:rating],
-            content: params[:review][:content],
-            username: params[:review][:username],
-            image: params[:review][:image],
-            shelter_id: params[:review][:shelter_id],
-            user_id: user.id
-        })
 
-    review.save
-    redirect_to "/shelters/#{review.shelter.id}"
+        @shelter = Shelter.find(params[:review][:shelter_id])
+        user = User.find_by(name: params[:review][:username])
+        if user == nil
+            flash[:notice] = "Review not created: Required information missing."
+            render :new
+        else
+            review = Review.find(params[:id])
+            user = User.find_by(name: params[:review][:username])
+            review.update({
+                title: params[:review][:title],
+                rating: params[:review][:rating],
+                content: params[:review][:content],
+                username: params[:review][:username],
+                image: params[:review][:image],
+                shelter_id: params[:review][:shelter_id],
+                user_id: user.id
+                      })
+            if review.save
+                redirect_to "/shelters/#{review.shelter.id}"
+            else
+                flash[:notice] = "Review not created: Required information missing."
+                render :new
+            end
+        end
     end
 
     def destroy
