3ea9238018b14571abc02c671699fc1b56a9a8ce
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-16 20:46:29 -0600

    Update spec to create pet, user and join table

--- spec/features/user_applications/show_spec.rb
+++ spec/features/user_applications/show_spec.rb
@@ -12,11 +12,32 @@ describe 'New Application' do
       city: 'Denver',
       state: 'Colorado',
       zip: '12345')  
-    
-    application = UserApplication.create!(name: "Jose", address: "", city: "", state: "", zip:"",
-      description: "", status: "", user_id: user_1.id)
+      shelter_1 = Shelter.create(name: 'Dumb Friends League',
+        address: '123 ABC Street',
+        city: 'Denver',
+        state: 'Colorado',
+        zip: '12345')
+      pet_1 = Pet.create(image: 'lib/assets/test_image',
+          name: 'Test_dog',
+          age: 5,
+          sex: 'male',
+          shelter_id: shelter_1.id)
 
+          
+      application = UserApplication.create!(name: "", address: "", city: "", state: "", zip:"",
+            description: "", status: "", user_id: user_1.id)
+      PetApplication.create(pet_id: pet_1.id, user_application_id: application.id)
+            
       visit "/applications/#{application.id}"
+
+      expect(page).to have_content(application.name)
+      expect(page).to have_content(application.address)
+      expect(page).to have_content(application.city)
+      expect(page).to have_content(application.state)
+      expect(page).to have_content(application.zip)
+      expect(page).to have_content(application.description)
+      expect(page).to have_content(application.status)
+
 
 
     end
