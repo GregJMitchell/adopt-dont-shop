a34e62b6c79632e2bfaa53c6344704482dd4260c
Jose Lopez <joselopez11394@gmail.com>
2020-10-16 21:12:21 -0400

    adds highleted reviews with test reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   def average_rating
     reviews.average(:rating)
   end
+
+  def best_review
+    (Review.where(rating: Review.maximum("rating")))[0]
+  end
+
+  def worst_review
+    (Review.where(rating: Review.minimum("rating")))[0]
+  end
 end
