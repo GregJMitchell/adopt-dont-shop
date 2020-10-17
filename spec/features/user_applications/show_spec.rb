2f9d5af212386859fbae0180073eec57cf73b589
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-17 17:14:31 -0600

    Add test for search for pets element

--- spec/features/user_applications/show_spec.rb
+++ spec/features/user_applications/show_spec.rb
@@ -42,5 +42,35 @@ describe 'New Application' do
 
 
     end
+    describe "And that application has not been submitted," do
+      it "Then I see a section on the page to Add a Pet to this Application 
+      In that section I see an input where I can search for Pets by name" do
+        user_1 = User.create(name: 'Jose',
+          address: '123 ABC Street',
+          city: 'Denver',
+          state: 'Colorado',
+          zip: '12345')  
+          shelter_1 = Shelter.create(name: 'Dumb Friends League',
+            address: '123 ABC Street',
+            city: 'Denver',
+            state: 'Colorado',
+            zip: '12345')
+          pet_1 = Pet.create(image: 'lib/assets/test_image',
+              name: 'Test_dog',
+              age: 5,
+              sex: 'male',
+              shelter_id: shelter_1.id)
+    
+              
+          application = UserApplication.create!(name: "", address: "", city: "", state: "", zip:"",
+                description: "", status: "", user_id: user_1.id)
+          PetApplication.create(pet_id: pet_1.id, user_application_id: application.id)
+        
+        visit "/applications/#{application.id}"
+
+        expect(page).to have_content("Search for Pets")
+        !find_field('pet[name]')
+      end
+    end
   end
 end
