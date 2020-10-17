6978a60fcc7056255bd142605ec435cafc45806f
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-16 19:29:28 -0600

    Create new tables for application and application pets

---
+++ db/migrate/20201017012636_create_application.rb
@@ -0,0 +1,12 @@
+class CreateApplication < ActiveRecord::Migration[5.2]
+  def change
+    create_table :applications do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :zip
+      t.string :description
+      t.string :status
+    end
+  end
+end
