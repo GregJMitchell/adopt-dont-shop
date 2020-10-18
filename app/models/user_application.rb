6824564dce2830fffc9170853929fea5f0631c27
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-17 20:15:51 -0600

    Add search class method to find pets

--- app/models/user_application.rb
+++ app/models/user_application.rb
@@ -3,4 +3,8 @@ class UserApplication < ApplicationRecord
   
   has_many :pet_applications
   has_many :pets, through: :pet_applications
+
+  def self.find_pets(name)
+    Pet.where(name: name).all
+  end
 end
