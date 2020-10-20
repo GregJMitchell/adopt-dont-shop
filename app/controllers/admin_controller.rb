e2007ac7638eabd2214ab045351f7e1a325d1d39
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-20 13:24:34 -0600

    Add logic to the controller to update the status if all are approved

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,15 +1,22 @@
 class AdminController < ApplicationController
   def show
     @application = UserApplication.find(params[:id])
-
   end
 
   def status
-    
-    application = PetApplication.find_by(pet_id: params[:pet], user_application_id: params[:id])
+    @application = UserApplication.find(params[:id])
+    pet_application = PetApplication.find_by(pet_id: params[:pet], user_application_id: params[:id])
 
-    application.update(status: params[:status])
-    
+    pet_application.update(status: params[:status])
+
+    if UserApplication.approved?(params[:id]) == 'pending'
+      return redirect_to "/admin/applications/#{params[:id]}"
+    elsif !UserApplication.approved?(params[:id])
+      @application.update(status: 'Rejected')
+    elsif UserApplication.approved?(params[:id])
+      @application.update(status: 'Approved')
+    end
+
     redirect_to "/admin/applications/#{params[:id]}"
   end
 end
