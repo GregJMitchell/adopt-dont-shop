392606b224125687db27dadee76853fb3526eacb
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-20 15:41:28 -0600

    Add index action to pet applications controller

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,4 +1,8 @@
 class PetApplicationsController < ApplicationController
+  def index
+    @pet_apps = PetApplication.where(pet_id: params[:id])
+  end
+
   def create
     match = PetApplication.new(pet_id: params[:pet][:pet_id], user_application_id: params[:id])
     match.save!
