8cef26c205fd24719162ec034b6e6d91703f6762
Jose Lopez <joselopez11394@gmail.com>
2020-10-20 19:48:41 -0400

    adds story 36

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,10 @@ class Pet <ApplicationRecord
   belongs_to :shelter
 
   has_many :user_applications, through: :pet_applications
+
+
+  def self.pet_approved?(pet)
+    apps = PetApplication.where(pet_id: pet)
+    apps.any? { |app| app.status == 'Approve' }
+  end
 end
