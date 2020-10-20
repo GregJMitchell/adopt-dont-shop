bc45e5f594da4cce13199fd1fb1a59e477bdb30a
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-20 17:59:44 -0600

    Create methods for shelter stats

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,8 @@ class Pet <ApplicationRecord
   belongs_to :shelter
 
   has_many :user_applications, through: :pet_applications
+
+  def self.count_pets(shelter_id)
+    Pet.where(shelter_id: shelter_id).count
+  end
 end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,4 +2,15 @@ class Shelter < ApplicationRecord
   has_many :pets, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :users, through: :reviews
+
+  def average_review_rating
+    reviews.average(:rating)
+  end
+
+  def count_applications
+    user_names = users.map do |user|
+      user.name
+    end
+    UserApplication.where(name: user_names).count
+  end
 end
